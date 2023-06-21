e67bc9e105df8f3a1baed285ce9185176d9ba7e0
vallafederico <valla.federico@gmail.com>
2023-06-21 12:37:08 +0200

    assets out

--- src/gl/assets/index.js
+++ src/gl/assets/index.js
@@ -1,12 +1,12 @@
-// import m_type from "./flr-mod.glb";
-import m_type from "./flr-asset-5.glb";
-import tx_mac from "./ipad_bake.png";
-import tx_rock from "./rock_bake.png";
-// import grid_tx from "./grid-800x494.jpg";
+const m_type =
+  "https://uploads-ssl.webflow.com/643d4a3e40f13a5d541373e9/6492d12e884c2e27f2a26fd0_flr-asset-5.glb.txt";
+const tx_mac =
+  "https://uploads-ssl.webflow.com/643d4a3e40f13a5d541373e9/6492d12ea817191a67fc0fa2_ipad_bake.webp";
+const tx_rock =
+  "https://uploads-ssl.webflow.com/643d4a3e40f13a5d541373e9/6492d12e2a448114fb96aa6b_rock_bake.webp";
 
 export const ASSETS = {
   m_type,
   tx_mac,
   tx_rock,
-  // grid_tx,
 };
